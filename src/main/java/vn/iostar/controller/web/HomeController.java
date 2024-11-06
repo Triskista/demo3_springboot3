84010db6ece1834fa6addb94dbfc7f8c3ec857af
Kim Trí Hồ <itztripvp@gmail.com>
2024-11-06 13:58:45 +0700

    adg

--- src/main/java/vn/iostar/controller/web/HomeController.java
+++ src/main/java/vn/iostar/controller/web/HomeController.java
@@ -1,11 +1,11 @@
 package vn.iostar.controller.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-	
-	@
+	@GetMapping("/")
 	public String index() {
 		return "web/home";
 	}
